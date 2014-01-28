3f429a238cbd9848c01d71619e76c84cecca5606
Quinn Rohlf <qr@qrohlf.com>
2014-01-28 12:13:19 -0800

    booyah!

--- labs/lab1.c
+++ labs/lab1.c
@@ -1,56 +1,113 @@
 #include <FPT.h>
 #include <D3d_matrix.h>
 
+// Constants
+
+    double h = .001;
+
 void circle(double p[3], double u) {
     p[0] = cos(u);
     p[1] = sin(u);
-    p[3] = 0;
+    p[2] = 0;
 }
 
 void sum4(double p[3], double u) {
     p[0] = sqrt(fabs(cos(u)))*((cos(u)<0)?-1.0:1.0); 
     p[1] = sqrt(fabs(sin(u)))*((sin(u)<0)?-1.0:1.0); 
-    p[3] = 0;
+    p[2] = 0;
 }
 
 void square(double p[3], double u) {
     p[0] = pow(cos(u), 2)*((cos(u)<0)?-1.0:1.0); 
     p[1] = pow(sin(u), 2)*((sin(u)<0)?-1.0:1.0); 
-    p[3] = 0;
+    p[2] = 0;
 }
 
 void astroid(double p[3], double u) {
     p[0] = pow(cos(u), 4)*((cos(u)<0)?-1.0:1.0); 
     p[1] = pow(sin(u), 4)*((sin(u)<0)?-1.0:1.0); 
-    p[3] = 0;
+    p[2] = 0;
 }
 
 void hyperbola(double p[3], double u) {
     p[0] = cosh(u);
     p[1] = sinh(u);
-    p[3] = 0;
+    p[2] = 0;
 }
 
 void parabola(double p[3], double u) {
     p[0] = u;
     p[1] = pow(u, 2);
-    p[3] = 0;
+    p[2] = 0;
 }
 
 void lemon(double p[3], double u) {
     p[0] = pow(cos(u), 3);
     p[1] = sin(u);
-    p[3] = 0;
+    p[2] = 0;
 }
 
-void graph(void (*func)(double[2], double), double m[4][4], double start, double end, double interval) {
+double magnitude(double vec[3]) {
+    return sqrt(pow(vec[0], 2)+pow(vec[1], 2)+pow(vec[2], 2));
+}
+
+void unit_vector(double a[3], double b[3]) {
+    double m = magnitude(a);
+    b[0] = a[0]/m;
+    b[1] = a[1]/m;
+    b[2] = a[2]/m;
+}
+
+//Gives slope vector for function
+void slope(void (*func)(double[3], double), double m[4][4], double u, double vec[3]) {
+    double p[3];
+    double x1, y1, x2, y2;
+    (*func)(p, u-h);
+    D3d_mat_mult_pt(p, m, p);
+    x1 = p[0];
+    y1 = p[1];
+    (*func)(p, u+h);
+    D3d_mat_mult_pt(p, m, p);
+    x2 = p[0];
+    y2 = p[1];
+    vec[0] = x2 - x1;
+    vec[1] = y2 - y1;
+    vec[2] = 0;
+}
+
+void graph_normals(void (*func)(double[3], double), double m[4][4], double start, double end, double interval) {
+    double p[3];
+    double vec[3];
+    double mat[4][4], minv[4][4];
+    double s;
+    for (double u=start+h*2; u<end-h*2; u+=interval) {
+        (*func)(p, u);
+        D3d_mat_mult_pt(p, m, p);
+        slope(func, m, u, vec);
+
+        //unit_vector(vec, vec);
+
+        // D3d_make_identity(mat);
+        // D3d_translate(mat, minv, -p[0], -p[1], -p[2]);
+        // D3d_rotate_z(mat, minv, -M_PI/2.0);
+        // D3d_translate(mat, minv, p[0], p[1], p[2]);
+        // D3d_mat_mult_pt(vec, mat, vec);
+
+        unit_vector(vec, vec);
+
+        
+        G_line(p[0], p[1], p[0]+vec[1]*30, p[1]-vec[0]*30);
+    }
+}
+
+void graph(void (*func)(double[3], double), double m[4][4], double start, double end, double interval) {
     double p[3];
     for (double u=start; u<end; u+=interval) {
         (*func)(p, u);
         D3d_mat_mult_pt(p, m, p);
         G_point(p[0], p[1]);
-
     }
+    graph_normals(func, m, start, end, interval*20);
 }
 
 int main(int argc, char const *argv[]) {
