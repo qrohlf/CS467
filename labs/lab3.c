ede7131aea33c62d700cb48af864c0e561e1adba
Quinn Rohlf <qr@qrohlf.com>
2014-04-22 10:31:03 -0700

    lab3 compiles!

--- labs/lab3.c
+++ labs/lab3.c
@@ -1,332 +1,179 @@
+// lab 3 (raytracing of pancakes)
 #include <FPT.h>
 #include <pi.h>
 #include <D3d_matrix.h>
 #include <limits.h>
 #include <vectors.h>
+#include <math.h>
 #include <light_model_student.h>
+#define false 0
+#define true 1
 
-
-// implicit equations:
-// unit sphere:
-// x^2+y^2+z^2-1 = 0
-// hyperboloid:
-// x^2/r^2 - y^2/b^2 + z^2/r^2 -1 = 0
 
 // Globals
-double Z_BUF[600][600];
 double VIEW[4][4], VIEW_INV[4][4];
 double fov;
 double R, G, B;
+int VERBOSE = false;
 
-typedef struct so
-{
-    void (*func)(double[3], double, double); 
-    double m[4][4]; 
-    double u0; // u lower bound
-    double uf; // u upper bound
-    double v0; // v lower bound
-    double vf;
-} scene_object;
+double spheres[4][4][4]; //sphere matrices
+double spheres_inv[4][4][4];
+double sphere_colors[4][3];
 
-scene_object SCENE[10];
-int SCENE_N = 0;
+void init_scene() {
+    int Tn;
+    int Ttypelist[10];
+    double Tvlist[10];
+    double (*Mat)[4] = spheres[0];
+    double (*Imat)[4] = spheres_inv[0];
+    //////////////////////////////////////////////
+    // unit sphere
 
-// Equation for the path of the camera
-void path (int frame_number, double path_xyz[3])
-{
-  double u,v,r ;
-  double x,y,z ;
+    Tn = 0 ; // number of transformations
+    Ttypelist[Tn] = SX ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = SY ; Tvlist[Tn] =   10    ; Tn++ ;
+    Ttypelist[Tn] = SZ ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = RX ; Tvlist[Tn] =  -70    ; Tn++ ;
+    Ttypelist[Tn] = TZ ; Tvlist[Tn] =  400    ; Tn++ ;
 
-  u = 5*frame_number*M_PI/180 ;
-  v = 0.3*u ;
-  r = 2.0 + 1.4*sin(u) ;
+    D3d_make_movement_sequence_matrix (
+        Mat,
+        Imat,
+        Tn,
+        Ttypelist,
+        Tvlist) ;
 
-  x = r*cos(u)*cos(v) ;
-  y = r*sin(u) ;
-  z = r*cos(u)*sin(v) ;
- 
-  path_xyz[0] = x ;
-  path_xyz[1] = y ;
-  path_xyz[2] = z ;
+    sphere_colors[0][0] = 0.00;
+    sphere_colors[0][1] = 0.00;
+    sphere_colors[0][2] = 1.00;
+    Mat = spheres[1];
+    Imat = spheres_inv[1];
 
+    ////////////////////////////////////////////////////
+    // unit sphere
+
+    Tn = 0 ; // number of transformations
+    Ttypelist[Tn] = SX ; Tvlist[Tn] =   10    ; Tn++ ;
+    Ttypelist[Tn] = SY ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = SZ ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = RY ; Tvlist[Tn] =    0    ; Tn++ ;
+    Ttypelist[Tn] = RX ; Tvlist[Tn] =  -70    ; Tn++ ;
+    Ttypelist[Tn] = TZ ; Tvlist[Tn] =  400    ; Tn++ ;
+
+    D3d_make_movement_sequence_matrix (
+        Mat,
+        Imat,
+        Tn,
+        Ttypelist,
+        Tvlist) ;
+
+
+    sphere_colors[1][0] = 0.00;
+    sphere_colors[1][1] = 1.00;
+    sphere_colors[1][2] = 0.25;
+    Mat = spheres[2];
+    Imat = spheres_inv[2];
+
+    ////////////////////////////////////////////////////
+    // unit sphere
+
+    Tn = 0 ; // number of transformations
+    Ttypelist[Tn] = SX ; Tvlist[Tn] =   10    ; Tn++ ;
+    Ttypelist[Tn] = SY ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = SZ ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = RY ; Tvlist[Tn] =   60    ; Tn++ ;
+
+    Ttypelist[Tn] = RX ; Tvlist[Tn] =  -70    ; Tn++ ;
+    Ttypelist[Tn] = TZ ; Tvlist[Tn] =  400    ; Tn++ ;
+
+    D3d_make_movement_sequence_matrix (
+        Mat,
+        Imat,
+        Tn,
+        Ttypelist,
+        Tvlist) ;
+
+
+
+    sphere_colors[2][0] = 1.00;
+    sphere_colors[2][1] = 1.00;
+    sphere_colors[2][2] = 1.00;
+    Mat = spheres[3];
+    Imat = spheres_inv[3];
+
+    ////////////////////////////////////////////////////
+    // unit sphere
+
+    Tn = 0 ; // number of transformations
+    Ttypelist[Tn] = SX ; Tvlist[Tn] =   10    ; Tn++ ;
+    Ttypelist[Tn] = SY ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = SZ ; Tvlist[Tn] =  100    ; Tn++ ;
+    Ttypelist[Tn] = RY ; Tvlist[Tn] =  120    ; Tn++ ;
+    Ttypelist[Tn] = RX ; Tvlist[Tn] =  -70    ; Tn++ ;
+    Ttypelist[Tn] = TZ ; Tvlist[Tn] =  400    ; Tn++ ;
+
+    D3d_make_movement_sequence_matrix (
+        Mat,
+        Imat,
+        Tn,
+        Ttypelist,
+        Tvlist) ;
+
+
+    sphere_colors[2][0] = 1.00;
+    sphere_colors[2][1] = 0.00;
+    sphere_colors[2][2] = 1.00;
 }
 
-// Takes the equation of a ray going from the origin in eyespace to (x, y, 1) in eyespace,
-// and finds the intersection between that ray and the objects in the scene
-// 
-// The ray equation in eyespace is in the form of t*(x/300, y/300, 1) = (x, y, z)
-// The scene equations in eyespace are in the form of f(u, v)*VIEW*m = (x, y, z)
-// Thus, t*(x/300, y/300, 1) = f(u, v)*VIEW*m
-void intersect() {
-
-}
-
-void set_color(double a, double b, double c) {
-    R = a;
-    G = b;
-    B = c;
-    G_rgb(R, G, B);
-}
-
-// flatten a 3d point into a 2d representation
-void flatten(double p[3]) {
-    p[0] = p[0]*(300/tan(fov))/p[2] + 300;
-    p[1] = p[1]*(300/tan(fov))/p[2] + 300;
-}
-
-void add_to_scene(
-    void (*func)(double[3], double, double), 
-    double m[4][4], 
-    double u0, // u lower bound
-    double uf, // u upper bound
-    double v0, // v lower bound
-    double vf) { // v upper bound
-    double p[3];
-    double irgb[3] = {R, G, B};     // Inherent color of object
-    double argb[3];                 // Actual color of object
-    double s[3] = {0, 0, 0};        // "s = location of start of ray"
-    double n[3];                    // normal vector
-    
-    //add to scene
-    scene_object* toAdd = &SCENE[SCENE_N];
-    toAdd->func = func; 
-    memcpy(&toAdd->m, &m, (sizeof(double[4][4])));
-    toAdd->u0 = u0;
-    toAdd->uf = uf;
-    toAdd->v0 = v0;
-    toAdd->vf = vf;
-    SCENE_N++;
-}
-
-// graph a sphere of radius r at point p
-void sphere_at(double p[3], double r) {
-    double m[4][4], m_inv[4][4];
-    int type[6];
-    double value[6];
-    
-    int n = 0 ; // number of transformations
-    type[n] = SX ; value[n] =  r    ; n++ ;
-    type[n] = SY ; value[n] =  r    ; n++ ;
-    type[n] = SZ ; value[n] =  r    ; n++ ;
-    type[n] = TX ; value[n] =  p[0]    ; n++ ;
-    type[n] = TY ; value[n] =  p[1]    ; n++ ;
-    type[n] = TZ ; value[n] =  p[2]    ; n++ ;
-
-    D3d_make_movement_sequence_matrix(m, m_inv, n, type, value);
-    D3d_mat_mult(m, VIEW, m);
-
-    add_to_scene(sphere, m, 0, M_PI*2.0, -M_PI/2.0, M_PI/2.0);
-}
-
-// hyperboloid at point p
-void hyp_base(double p[3], double length, double rz, double ry) {
-    double m[4][4], m_inv[4][4];
-    double ab[3]; //the vector between the two points
-    int type[100];
-    double value[100];
-    
-    int n = 0 ; // number of transformations
-
-    // make the hyperboloid thinner
-    type[n] = SX ; value[n] =  0.03    ; n++ ;
-    type[n] = SZ ; value[n] =  0.03    ; n++ ;
-
-    // get it on top of the x axis
-    type[n] = TY; value[n] = 1; n++;
-
-    // fit it to the right length
-    type[n] = SY ; value[n] =  length/2.0    ; n++ ;
-
-    // rotate
-    type[n] = RZ ; value[n] =  rz    ; n++ ;
-    type[n] = RY ; value[n] =  ry    ; n++ ;
-
-    // translate to point p
-    type[n] = TX ; value[n] =  p[0]    ; n++ ;
-    type[n] = TY ; value[n] =  p[1]    ; n++ ;
-    type[n] = TZ ; value[n] =  p[2]    ; n++ ;
-
-    D3d_make_movement_sequence_matrix(m, m_inv, n, type, value);
-    D3d_mat_mult(m, VIEW, m);
-
-    add_to_scene(hyperboloid, m, -1, 1, 0, M_PI*2.0);
-}
-
-void hyp_top(double length, double ry, double rz) {
-    double m[4][4], m_inv[4][4];
-    double ab[3]; //the vector between the two points
-    int type[100];
-    double value[100];
-    
-    int n = 0 ; // number of transformations
-
-    // make the hyperboloid thinner
-    type[n] = SX ; value[n] =  0.03    ; n++ ;
-    type[n] = SZ ; value[n] =  0.03    ; n++ ;
-
-    // get it on top of the x axis
-    type[n] = TY; value[n] = 1; n++;
-
-    // fit it to the right length
-    type[n] = SY ; value[n] =  length/2.0    ; n++ ;
-
-    // tilt
-    type[n] = RZ ; value[n] =  rz; n++ ; 
-
-    // translate to first vertex
-    type[n] = TX ; value[n] =  1    ; n++ ;
-    type[n] = TY ; value[n] =  0    ; n++ ;
-    type[n] = TZ ; value[n] =  0    ; n++ ;
-
-    // rotate
-    type[n] = RY ; value[n] =  ry    ; n++ ;
-
-    D3d_make_movement_sequence_matrix(m, m_inv, n, type, value);
-    D3d_mat_mult(m, VIEW, m);
-
-    add_to_scene(hyperboloid, m, -1, 1, 0, M_PI*2.0);
-}
-
-
-// Scene construction and rendering isn't seperated here because
-// the assumption is that we'll be using out-of-process 
-// parallelization
 void render(int frame_number) {
-    // ################################
-    // 0. Black background
-    // ################################
-    SCENE_N = 0; //reset the scene
-    set_color(0, 0, 0);
+    G_rgb(0, 0, 0);
     G_clear();
+    fov = (80-2*frame_number)*M_PI/180;
+    
 
-    // ################################
-    // 1. Set the view matrix
-    // ################################
-    double eye[3], coi[3], up[3];
+    ////////////////////////////////////////////////////
 
-    // set eye point
-    path (frame_number, eye);
+}
 
-    // set center of interest
-    coi[0] = 0;
-    coi[1] = 0.25;
-    coi[2] = 0;
+int main(int argc, char const *argv[]) {
+    char prefix[100], sequence_name[100] ;
+    int s,e,i ;
+    double f ;
 
-    // set the up point
-    path (frame_number + 1, up) ; 
+    if (argc == 4) {
+        strcpy(prefix, argv[1]);
+        s = atoi(argv[2]);
+        e = atoi(argv[3]);
+    } else {
+        printf("enter prefix name ") ;
+        scanf("%s",prefix) ;
 
-    // compute the view matrix
-    D3d_view(VIEW, VIEW_INV, eye, coi, up); 
+        printf("enter starting integer ") ;
+        scanf("%d",&s) ;
 
-    // ################################
-    // 2. Set the light model
-    // ################################
-    double light_position_world[3];
+        printf("enter ending integer ") ;
+        scanf("%d",&e) ;
+    }
 
-    // set constants
+    
+
+    G_init_graphics(600, 600);
+
+    init_scene();
+
+    //Set up light
+    light_in_eye_space[0] = 100 ; light_in_eye_space[1] = 200 ; light_in_eye_space[2] = -50 ;
     AMBIENT  = 0.2 ;
     MAX_DIFFUSE = 0.5 ;
     SPECPOW = 80 ;
-    fov = 25 * M_PI/180;
 
-    // get the light's position in world space
-    path (frame_number + 10, light_position_world);
-
-    // set the light's position in eye space
-    D3d_mat_mult_pt(light_in_eye_space, VIEW, light_position_world);
-
-    // ################################
-    // 3. Build the tetrahedron vertices
-    // 
-    // tetrahedron side length is sqrt(3)
-    // tetrahedron height is 
-    // ################################
-    double vertices[4][3];
-    double vertex_radius = 0.08;
-
-    // compute the locations of the base
-    for (int i = 0 ; i < 3 ; i++) {
-        double theta = M_PI*i*2.0/3.0;
-        vertices[i][0] = cos(theta);
-        vertices[i][1] = 0;
-        vertices[i][2] = sin(theta);
-    }    
-
-    // set the location of the top
-    vertices[3][0] = 0;
-    vertices[3][1] = sqrt(2);
-    vertices[3][2] = 0;
-
-    // graph all the vertices
-    set_color(0.00, 0.00, 1.00);
-    for (int i=0; i<4; i++) {
-        sphere_at(vertices[i], vertex_radius);
-    }
-
-    // ################################
-    // 4. Build the tetrahedron edges
-    // ################################
-    set_color(0.00, 1.00, 1.00);
-
-    // connect the three base vertices to each other and the top
-    double rot[3] = {30, 270, 150};
-    for (int i=0; i<3; i++) {
-        hyp_base(vertices[i], sqrt(3), 90, rot[i]);
-        hyp_top(sqrt(3), rot[i]-30, 35.26); //acos(sqrt(2/3)) == 35.26 (in degrees)
-    }
-
-    // ################################
-    // 5. Build the tetrahedron center
-    // ################################
-    double center_radius  = 0.20;
-    set_color(1.00, 0.50, 0.00);
-    sphere_at(coi, center_radius);
-    // set_color(1,1,1); sphere_at(coi, center_radius/3.0); //debug only
-
-    // ################################
-    // 6. connect the vertices to the center
-    // ################################
-    set_color(1.0, 1.0, 1.0);
-    double length = sqrt(pow(.25, 2)+1);
-    double angle = 90 - (atan(0.25)*180.0/M_PI);
-    //the base
-    for (int i=0; i<3; i++) {
-        hyp_top(length, rot[i]-30, angle); //acos(sqrt(2/3)) == 35.26 (in degrees)
-    }
-    //and the top
-    hyp_base(coi, length, 0, 0);
-
-}
-
-
-// usage: lab2.1 [prefix] [framenumber]
-int main(int argc, char const *argv[]) {
-    char prefix[100], sequence_name[100];
-    int start, end;
-
-
-    //Read the prefix and frame number from argv
-    strcpy(prefix, argv[1]); 
-    start = atoi(argv[2]); 
-    if (argc == 4) {
-        end = atoi(argv[3]);
-    } else {
-        end = start;
-    }
-
-    //Initialize the graphics
-    G_init_graphics(600, 600);    
-
-    for (int i=start; i<=end; i++) {
-        printf("rendering frame %d...\n", i);
-        // Render the image
+    for (i = s ; i <= e ; i++) {
         render(i);
-        printf("done rendering frame %d\n", i);
-
-        // Save the image
         sprintf(sequence_name, "%s%04d.xwd", prefix, i) ;
-        G_save_image_to_file(sequence_name) ;
+        // G_save_image_to_file(sequence_name) ;
+        printf("done rendering frame %d\n", i);
     }
+
     G_close() ;
     return 0;
 }
